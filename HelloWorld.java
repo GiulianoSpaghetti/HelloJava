f87c5abefbb7bda8a4b4208b59ec1fd8f3313601
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2022-08-04 08:39:46 +0200

    addedd internazionalization

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,15 +3,18 @@ package com.github.numerunix;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 /* Questo codice è sotto licenza MIT*/
 public class HelloWorld {
 	
 	public static void main(String args[]) {
+		ResourceBundle bundle=ResourceBundle.getBundle("Hello", Locale.getDefault());
 		String s="";
 		byte[] b=new byte[256];
 		InputStream r=System.in;
-		System.out.print("Inserisci il tuo nome: ");
+		System.out.print(bundle.getString("InserireNome")+": ");
 		try {
 			r.read(b);
 		} catch (IOException e) {
@@ -19,7 +22,7 @@ public class HelloWorld {
 			e.printStackTrace();
 		}
 		s=new String(b).strip();
-		System.out.print("Ciao "+s+" dedico questo progetto alla famiglia San Severino.");
+		System.out.print(bundle.getString("Ciao")+" "+s+" "+bundle.getString("dedica")+".");
 		try {
 			r.read(b);
 		} catch (IOException e) {
